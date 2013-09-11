c5166f812eddc7ca07a3e01ae62fc515f46d7e2e
John S. Ryan <jtigger@infosysengr.com>
2013-09-11 10:24:47 -0700

    Ch2. Simple Controller.

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,29 @@
 require "rulers/version"
+require "rulers/routing"
 require "rulers/array"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+      
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      reponse_body = controller.send(action)
+      [200, {'Content-Type' => 'text/html'}, [reponse_body]]
     end
+  end
+  
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    
+    def env
+      @env
+    end
+    
   end
 end
