2a03b6223903bdd53a88dfafa099cfacf10ec073
John S. Ryan <jtigger@infosysengr.com>
2013-09-11 15:29:46 -0700

    Now Controllers are dynamically loaded from files, detecting missing classes.
    That is: catches (and reports) the condition where a Ruby file is found, but that file does not contain a class definition of the same name as the file. (e.g. foo_bar.rb not containing class FooBar).
    
    + also removed unnused Array.sum method.

--- lib/rulers/array.rb
+++
@@ -1,7 +0,0 @@
-# lib/rulers/array.rb
-
-class Array
-  def sum(start = 0)
-    inject(start, &:+)
-  end
-end
--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,12 @@
 # /lib/rulers/dependencies.rb
 class Object
   def self.const_missing(c)
+    # if the last line of this method fails to get the class definition, this method is invoked again.
+    penultimate_caller = caller[1][/`([^']*)'/,1]
+    if penultimate_caller == "const_missing"
+      raise "Found file #{Rulers.to_underscore(c.to_s)}.rb, but failed to find #{c.to_s} in it (is either the filename or classname misspelled?)."
+    end
+
     require Rulers.to_underscore(c.to_s)
     Object.const_get(c)
   end
