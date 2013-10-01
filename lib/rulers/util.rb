a976e84329b3ae08bb3e7ee50afb92449e3d94d4
John S. Ryan <jtigger@infosysengr.com>
2013-10-01 07:44:23 -0700

    Extracted 'snake casing' to a monkey patch on String.

--- lib/rulers/util.rb
+++ lib/rulers/util.rb
@@ -2,10 +2,6 @@
 
 module Rulers
   def self.to_snakecase(string)
-    string.gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
-      gsub(/([a-z\d])([A-Z])/, '\1_\2').
-      tr("-", "_").
-      downcase
+    string.to_snake_case
   end
 end
