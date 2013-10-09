bdea8528b4e5b312b879b590cc346cd265e1521b
John S. Ryan <jtigger@infosysengr.com>
2013-10-08 18:27:41 -0700

    SQLiteModel SQL mapping logic handles nil and hashes
    + generating the SQL string
    + sketching what create() will look like

--- lib/rulers/model/sqlite_model.rb
+++ lib/rulers/model/sqlite_model.rb
@@ -2,6 +2,7 @@
 
 module Rulers
   module Model
+    
     class SQLiteModel
       def self.connect(path_to_db)
         @db = SQLite3::Database.new path_to_db
@@ -18,16 +19,41 @@ module Rulers
         end
         @schema
       end
-
+      
+      def self.create(values)
+        # SQL escape values
+        # generate SQL for insert
+        # execute insert
+        # obtain ID    select_rowid_sql = "SELECT last_insert_rowid();"; @db.execute select_rowid_sql
+        # "unescape" SQL into Model values
+        # scatter results into an Model instance variables
+        
+        insert_sql = sql_for_create Hash[schema.keys.zip(to_sql(values))]
+        @db.execute insert_sql
+      end
+      
       def self.to_sql(value)
         case value
+        when Hash
+          keys = schema.keys - ["id"]
+          keys.map do |key|
+            value_for_key = value[key]
+            value_for_key = value[key.to_sym] if !value_for_key
+            to_sql(value_for_key)
+          end
         when Numeric
           value.to_s
         when String
           "'#{value}'"
+        when nil
+          "NULL"
         else
           raise "Unable to map #{value.class} to SQL."
         end
+      end
+      
+      def self.sql_for_create(column_value_pairs)
+        "INSERT INTO #{table} (#{column_value_pairs.keys.join(", ")}) values (#{column_value_pairs.values.join(", ")});"
       end
     end
   end
--- test/test_sqlite_model.rb
+++ test/test_sqlite_model.rb
@@ -5,6 +5,8 @@ require "sqlite3"
 class TestSqliteModel < Rulers::Model::SQLiteModel; end
 
 class RulersSQLiteModelTest < Test::Unit::TestCase
+
+  # GIVEN: a concrete subclass of SQLiteModel, connected to a real table with a few columns on it...
   def setup
     @build_test_temp_dir = "build/test"
     database_filename = "#{@build_test_temp_dir}/test.db"
@@ -26,7 +28,7 @@ __
   def teardown
     `rm -rf #{@build_test_temp_dir}`
   end
-  
+    
   def test_SQLiteModel_assumes_the_backing_table_matches_the_classname
     assert_equal "test_sqlite", TestSqliteModel.table
   end
@@ -36,12 +38,40 @@ __
     assert_equal expected_schema, TestSqliteModel.schema
   end
   
-  def test_WHEN_generating_sql_SQLiteModel_translates_integer_values_to_Strings
+  def test_WHEN_generating_sql_SQLiteModel_translates_integer_values_to_strings
     assert_equal "42", TestSqliteModel.to_sql(42)
   end
   
   def test_WHEN_generating_sql_SQLiteModel_surrounds_strings_with_single_quotes
     assert_equal "'Hello, world!'", TestSqliteModel.to_sql("Hello, world!")
   end
+  
+  def test_WHEN_generating_sql_SQLiteModel_translates_nil_to_null
+    assert_equal "NULL", TestSqliteModel.to_sql(nil)
+  end
+  
+  def test_WHEN_generating_sql_SQLiteModel_escapes_hashes_of_values
+    values = { :name => "Lily G", :age => 0, :tagline => "Ooooooooohhh!" }
+    sql_escaped_values = ["'Lily G'", "0", "'Ooooooooohhh!'"]
+    
+    assert_equal sql_escaped_values, TestSqliteModel.to_sql(values)
+  end
+
+  def test_WHEN_generating_sql_SQLiteModel_inserts_null_for_missing_values
+    values = { :name => "Lily G",
+             # :age => 0,   -- this value is intentionally removed
+               :tagline => "Ooooooooohhh!" }
+    sql_escaped_values = ["'Lily G'", "NULL", "'Ooooooooohhh!'"]
+    
+    assert_equal sql_escaped_values, TestSqliteModel.to_sql(values)
+  end
+  
+  def test_WHEN_generating_sql_to_create_a_new_model_SQLiteModel_emits_a_SQL_insert
+    expected_sql = "INSERT INTO test_sqlite (name, age, tagline) values ('Lily G', 0, 'Ooooooooohhh!');"
+    values = { :name => "'Lily G'", :age => '0', :tagline => "'Ooooooooohhh!'" }
+    
+    assert_equal expected_sql, TestSqliteModel.sql_for_create(values)
+  end
+
 end
 
